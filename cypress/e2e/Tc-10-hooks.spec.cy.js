03351a5846bb15983a66004ff1da5ee75bd5f9dd
lakshmi-devi <lakshmi.d@access-designs.in>
2024-12-19 13:56:27 +0530

    initial commit

--- cypress/e2e/Tc-10-hooks.spec.cy.js
+++ cypress/e2e/Tc-10-hooks.spec.cy.js
@@ -11,6 +11,9 @@ describe('hooks functions', function(){
     it('before and beforeeach hooks 2', function(){
         cy.log('inside it2')
     })
+    it('before and beforeeach hooks 2', function(){
+        cy.log('inside ieasddd2')
+    })
 
     afterEach(function(){
         cy.log('after each it')
